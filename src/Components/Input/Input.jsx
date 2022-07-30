57d722d227b08ccd8c0ac5c7c2ae413fcbdc2e8d
Nathan Gilbert <weneedmoreminerals@gmail.com>
2022-07-29 22:06:13 -0400

    Quick Fix for Input

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import "./Input.css";
 
-const Input = ({ label, type, required }) => {
+const Input = ({ label, type }) => {
   if (!type) type = "text";
   if (!label) label = "";
   return (
     <div className="text-input-box">
       <div className="text-input-inner">
-        <input
-          className="input-field"
-          type={type}
-          name=""
-          required={required}
-        />
+        <input className="input-field" type={type} name="" required />
         <label className="input-label">{label}</label>
       </div>
     </div>
