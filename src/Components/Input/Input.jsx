b6dd23215c9c818ddf54e0e17c723a4e4ab6af87
Nathan Gilbert <weneedmoreminerals@gmail.com>
2022-07-30 16:52:01 -0400

    Modularize Input and fixed paste background color

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Input.css";
 
-const Input = ({ label, type }) => {
+const Input = ({ label, type, value, name, onChange }) => {
   if (!type) type = "text";
   if (!label) label = "";
   return (
     <div className="text-input-box">
       <div className="text-input-inner">
-        <input className="input-field" type={type} name="" required />
+        <input
+          className="input-field"
+          type={type}
+          name={name}
+          value={value}
+          required
+          onChange={(e) => onChange(e)}
+        />
         <label className="input-label">{label}</label>
       </div>
     </div>
